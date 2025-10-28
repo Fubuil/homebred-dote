41658366339e8a43b2194f3cca3499bd36ab5794
Fubuil <yuriko0221symr@icloud.com>
2025-10-28 11:30:44 +0900

    Fix formatting of version line in dote.rb

--- dote.rb
+++ dote.rb
@@ -3,7 +3,7 @@ class Dote < Formula
   homepage "https://github.com/Fubuil/homebrew-dote"
   url "https://raw.githubusercontent.com/Fubuil/homebrew-dote/main/dote.c"
   sha256 "6f671f49bd565957845cc9f44a9ccd951cdb8eda33e1fbd3b32be95b361419db"
-  version"1.0.0"
+  version "1.0.0"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
